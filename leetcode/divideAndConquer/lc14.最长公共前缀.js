22444d5dd66a616d252c5e5c6207bdaf8047ac29
Ashlyn1997 <ashlyn1997@163.com>
2021-09-22 10:07:10 +0800

    最长公共前缀

--- leetcode/divideAndConquer/lc14.最长公共前缀.js
+++ leetcode/divideAndConquer/lc14.最长公共前缀.js
@@ -4,7 +4,7 @@
  */
 
 // 暴力解
-var longestCommonPrefix = function(strs) {
+var longestCommonPrefix1 = function(strs) {
     let res = ''
     if (!strs.length) return res
     for (let j=0; j < strs[0].length; j++) {
@@ -17,3 +17,25 @@ var longestCommonPrefix = function(strs) {
 }
 
 // 分治
+let longestCommonPrefix = function(strs) {
+    if (strs === null || strs.length === 0) return "";
+    return lCPrefixRec(strs)
+}
+function lCPrefixRec(arr) {
+    let length = arr.length
+    if (length === 1) return arr[0]
+    let mid = Math.floor(length / 2)
+    let left = lCPrefixRec(arr.slice(0, mid))
+    let right = lCPrefixRec(arr.slice(mid, length))
+    return lCPrefixTwo(left, right)
+}
+
+function lCPrefixTwo(str1, str2) {
+    let j = 0
+    for (; j < str1.length && j < str2.length; j++) {
+        if (str1[j] != str2[j]) {
+            break
+        }
+    }
+    return str1.substring(0, j)
+}
